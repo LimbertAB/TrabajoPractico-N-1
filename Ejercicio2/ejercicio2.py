3f7ec08f1d69d5f90a20c389e4638010ad348072
LimbertAB <limbert_90_lab@hotmail.com>
2014-09-03 21:43:00 -0400

    ejercicio2

---
+++ Ejercicio2/ejercicio2.py
@@ -0,0 +1,16 @@
+ini=int(input())
+fin=int(input())
+aux=0
+aux2=0
+aux=ini
+aux2=fin
+while ini <= fin: 
+   print ini,
+   ini+=1 
+print""
+while aux<=aux2:
+	if aux % 2 ==0:
+		   print aux,
+		   aux+=1
+	else:
+ 		aux+=1
